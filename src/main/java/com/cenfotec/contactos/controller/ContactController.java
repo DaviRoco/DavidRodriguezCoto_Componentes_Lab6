3fc667860920cad8a8aeb4d10083d5fc7dc3e32c
David <drodriguezc1@ucenfotec.ac.cr>
2022-04-05 21:15:29 -0600

    Lab 6

--- src/main/java/com/cenfotec/contactos/controller/ContactController.java
+++ src/main/java/com/cenfotec/contactos/controller/ContactController.java
@@ -37,7 +37,7 @@ public class ContactController {
         return contactService.save(contact).get();
     }
 
-    @PutMapping(value="/{id}")
+//    @PutMapping(value="/{id}")
     public ResponseEntity<Contact> update(@PathVariable("id") long id,
                                           @RequestBody Contact contact){
         contact.setId(id);
@@ -63,9 +63,9 @@ public class ContactController {
 //    a. Cuál es la diferencia en la práctica entre @Controller y @RestController?
 //          @Controller es usada para establecer una clase como Controller en SpringMVC mientras @RestController es un controller especial en RESTful y equivale a la suma de @Controller y @ResponseBody.
 //    b. Cuál es mejor usar en qué casos?
-//          @RestController es mejor usarlo cuando se requiere de la metodología REST mientras que @Controller
+//          @RestController es mejor usarlo cuando se requiere de la metodología REST mientras que @Controller indica que la clase es un controlador web
 //    c. Por qué en el findAll no se especifica el URI para la operación?
-//        En findAll no se especifica el URL para la operación porque en el service se hace el findAll, se trae una lista de Contact.
+//        En findAll no se especifica el URI porque esa es la manera default, no se utilizan recursos.
 //    d. Qué obtiene si trata de borrar 2 veces un recurso?
 //        Nada, la pantalla sale en negro.
 //    e. Comente la anotación de PutMapping del método que hace el update. Reinicie la aplicación. Observe que pasa si lo trata de hacer.
